7a6a546d163a759559df2884f83b8e872fc7a6a2
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2024-04-22 10:09:55 +0530

    testing

---
+++ ZZZZZ_UI_Automation_practice/src/main/java/UI_Automation_practice/UI_Automation_practice/testing.java
@@ -0,0 +1,10 @@
+package UI_Automation_practice.UI_Automation_practice;
+
+public class testing 
+{
+	public static void main(String [] args)
+	{
+		System.out.println("testing ---");
+	}
+
+}
---
+++ ZZZZZ_UI_Automation_practice/src/main/java/textBox_sendingData/Textbox.java
@@ -0,0 +1,14 @@
+package textBox_sendingData;
+
+import org.testng.annotations.Test;
+
+public class Textbox 
+{
+	@Test
+	public void inputdataTo_textBox()
+	{
+		System.out.println("this is test ng");
+		
+	}
+
+}
