533106305910bda68530b20a481851c534d1297c
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2024-04-22 13:53:19 +0530

    testing ui

---
+++ Automation_practice_UI/src/main/java/Automation_practice_UI/Automation_practice_UI/App.java
@@ -0,0 +1,13 @@
+package Automation_practice_UI.Automation_practice_UI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
