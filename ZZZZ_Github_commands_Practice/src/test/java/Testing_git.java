ca997d8a2de5a2a39e4c5419615fbaf49f4d3f51
Ananda2000 <41889321+Ananda2000@users.noreply.github.com>
2024-12-13 08:47:53 +0530

    Update Testing_git.java

--- ZZZZ_Github_commands_Practice/src/test/java/Testing_git.java
+++ ZZZZ_Github_commands_Practice/src/test/java/Testing_git.java
@@ -5,6 +5,8 @@ public class Testing_git {
 		System.out.println("this is testing..");
 		// this is commintg on 13-24-dec
 		System.out.println(" 13-24-dec commited");
+		// This is edited in github to check pull request.
+		System.out.println(" 13-24-dec for pull request checking");
 	}
 
 }
