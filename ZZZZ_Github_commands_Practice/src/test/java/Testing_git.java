bb4a00d35e52566f5b99042a155b93dbf045580a
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2024-12-13 08:41:37 +0530

    this commiting on 13-24-dec

--- ZZZZ_Github_commands_Practice/src/test/java/Testing_git.java
+++ ZZZZ_Github_commands_Practice/src/test/java/Testing_git.java
@@ -1,10 +1,10 @@
 package test.java;
 
-public class Testing_git 
-{
-	public static void main(String [] args)
-	{
+public class Testing_git {
+	public static void main(String[] args) {
 		System.out.println("this is testing..");
+		// this is commintg on 13-24-dec
+		System.out.println(" 13-24-dec commited");
 	}
 
 }
