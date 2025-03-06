4ffa31d09ca9e4589c669e9159f496b8f619adc1
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2025-03-06 09:13:28 +0530

    Adding untracked files

--- ZZZZ_Github_commands_Practice/src/src/main/java/package1/Test1_Class.java
+++ ZZZZ_Github_commands_Practice/src/src/main/java/package1/Test1_Class.java
@@ -1,24 +1,22 @@
 package src.main.java.package1;
 
-public class Test1_Class 
-{
-	
-	public static void main(String [] args)
-	{
+public class Test1_Class {
+
+	public static void main(String[] args) {
 		System.out.println("This is called testing...");
-		//this is testing
+		// this is testing
 		// This is anand..
 
 		// this is after fetch...
 
 		// This is for fetch testing..
-		
+
 		// Updating on 10-8-2024..
-		
-		
+
 		// Edited in eclispe
 
-	// This is edited in github
+		// This is edited in github
+		System.out.println("This is merge conflict...");
 
 	}
 
