b07724bc8a68dbd8e75bf5747da31786727584ca
Ananda Ramakrishna <ramakrishnaka99@gmail.com>
2025-12-19 18:20:49 +0530

    commiting canwrite and Delete Methods of File class

--- Java__Practicing/FileHandling/file_Class/Canwrite_Method.java
+++ Java__Practicing/FileHandling/file_Class/Canwrite_Method.java
@@ -1,10 +1,21 @@
 package file_Class;
 
+import java.io.File;
+
 public class Canwrite_Method 
 {
 	public static void main(String [] args)
 	{
-		
+		File fl = new File("D:\\FileHandlingPRactice\\create_newFile.txt");
+		boolean can_write = fl.canWrite();
+		if(can_write)
+		{
+			System.out.println("Can successfully write into File");
+		}
+		else
+		{
+			System.out.println("Cant able to write into File --");
+		}
 	}
 
 }
---
+++ Java__Practicing/FileHandling/file_Class/Delete_method.java
@@ -0,0 +1,5 @@
+package file_Class;
+
+public class Delete_method {
+
+}
